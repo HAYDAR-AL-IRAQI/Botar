330132e92d1ad1045b34ddd269d6aba8585f3415
HAYDAR AL-IRAQI <49880254+HAYDAR-AL-IRAQI@users.noreply.github.com>
2021-03-26 01:26:44 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$TOKEN = ""; // bot token
-$botUsername = ""; // bot username without @
+$TOKEN = "1774634567:AAGYlC0Azc1rDQj7OTUtj7Bx_3nzcQjNw-4"; // bot token
+$botUsername = "SWEET_IRAQI_bot"; // bot username without @
 
 /*
 $databaseHost = "localhost";
